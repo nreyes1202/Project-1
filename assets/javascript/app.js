9bcf224da50ac09fec24fbda6e3616e40e0d4575
Miranda D'Asto <merlinockipella@gmail.com>
2020-02-11 19:57:06 -0700

    Sending code to firebase and console logging to ensure accuracy.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,40 @@ var database = firebase.database();
 
 //Creating new to-do when submit button is clicked.
 
+$("#submitButton").on("click", function(event)
+{
+    event.preventDefault();
+
+    //grabs user input
+    var userToDo = $("#toDoInput").val().trim();
+    var toDoCategory = $("#categoryInput").val().trim();
+    var toDoColor = $("#colorOfInput").val().trim();
+    var toDoImportanceLevel = $("#importanceLevel").val().trim();
+    var toDoDueDate = $("#dateDueInput").val().trim();
+
+    //Creates local "temporary" object for holding new to-do data
+    var newToDo = {
+        todo: userToDo,
+        category: toDoCategory,
+        color: toDoColor,
+        importance: toDoImportanceLevel,
+        date: toDoDueDate
+    };
+
+    //Uploads new to-do to the database
+    database.ref().push(newToDo);
+
+    //logs everything to console
+    console.log(newToDo.todo);
+    console.log(newToDo.category);
+    console.log(newToDo.color);
+    console.log(newToDo.importance);
+    console.log(newToDo.date);
+
+    //clears all text-boxes
+    $("#toDoInput").val("");
+    $("#categoryInput").val("");
+    $("#colorOfInput").val("");
+    $("#importanceLevel").val("");
+    $("#dateDueInput").val("");
+});
