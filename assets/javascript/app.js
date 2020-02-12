477d05d7e7ae1d245c8cd46dbb21fedfff4925e6
Miranda D'Asto <merlinockipella@gmail.com>
2020-02-11 19:40:37 -0700

    Initialized firebase in javascript, pulled in firebase library into html. added a tbody tag to html. pulled in moment library to html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-function()
+//initializing firebase
+var config = {
+    apiKey: "AIzaSyA2QGrQQuHKU30S6xrM9MFwcUzjlcwjltQ",
+    authDomain: "to-do-or-not-to-do-list.firebaseio.com/",
+    databaseURL: "https://to-do-or-not-to-do-list.firebaseio.com/"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//Creating new to-do when submit button is clicked.
+
